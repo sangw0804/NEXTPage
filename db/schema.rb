4a579c7c5359b549c3a697eff5b740db773663c0
SangWoo <sangw0804@likelion.org>
2018-05-13 00:40:56 +0900

    user post model relate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180512053621) do
+ActiveRecord::Schema.define(version: 20180512145629) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20180512053621) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
